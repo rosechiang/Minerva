8ed2999ab6d4e4a47eb2079c30824117028a5e7e
rosechiang <hsiaojuc@usc.edu>
2023-04-22 18:10:48 -0700

    mac to window
    1. change file name
    2. change path
    3. root path reading minerva author from one drive

--- Convert_windowjson_to_macjson.py
+++ Convert_windowjson_to_macjson.py
@@ -1,20 +1,26 @@
 import json
-import codecs
-# from mac
+
+# from mac in mac
 # path = '/Users/rose/Documents/OneDrive - Akoya Biosciences/Documents/Minerva/08-Breast/'
-# from window
+# from window to mac
 path = '/Users/rose/Library/CloudStorage/OneDrive-AkoyaBiosciences/Documents/Minerva/08-Breast/'
-file_path = path + '08-breast_cancer_window0420.story copy.json'
-output_file_path = path + '08-breast_cancer_window0420.story copy.json'
-new_in_file_path = path + "20221220_S1614455_C21_Scan1.ome.tiff"
-marker_path = path + "20221220_S1614455_C21_Scan1_marker.csv"
-rootpath = "/Users/rose/Library/CloudStorage/OneDrive-AkoyaBiosciences/Documents/minerva_author_macos"
+# from mac to window
+path = "C:/Users/hjchiang/OneDrive - Akoya Biosciences/Documents/Minerva/09-Pancrease/"
+# "C:\Users\hjchiang\OneDrive - Akoya Biosciences\Documents\Minerva\09-Pancrease\out_mac0418.story.json"
+
+file_path = path + 'out_mac0418.story.json'
+output_file_path = path + 'out_0422window.story.json'
+new_in_file_path = path + "20230110_MGH PDAC_1B_30px_HJ_Scan1.ome.tiff"
+marker_path = path + "MarkerList_MGH PDAC 1B.csv"
+
+rootpath_mac = "/Users/rose/Library/CloudStorage/OneDrive-AkoyaBiosciences/Documents/minerva_author_macos"
+rootpath_window = "C:/Users/hjchiang/OneDrive - Akoya Biosciences/Documents/minerva_author_macos"
 with open(file_path, "r") as f:
     data = json.load(f)
 
 data["csv_file"] = marker_path
 data["in_file"] = new_in_file_path
-data["root_dir"]= rootpath
+data["root_dir"]= rootpath_window
 
 with open(output_file_path, "w") as f:
     json.dump(data, f)
